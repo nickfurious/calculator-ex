4236f36632cdccc9180b2c004d70658f9196b05c
nickfurious <datfuriouskid@live.com>
2021-05-03 21:29:05 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+class Calculator {
+
+  constructor(name) {
+    this.name = name;
+    this.result = 0;
+  }
+
+  add(num) {
+    // sumation code
+  }
+
+  subtraction(num) {
+    // subtraction code
+  }
+
+  clear() {
+    // clear result
+  }
+
+  print() {
+    console.log(this.result)
+  }
+}
+
+// clear and instantiate class
+let calc = new Calculator()
+
+// have fun with the Calculator
